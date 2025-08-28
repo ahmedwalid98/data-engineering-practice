cc4c222467c6bc5ae7cc4cb4648739568d89086f
Ahmed Walid <ahmedawlid98.aw@gmail.com>
2025-08-28 17:05:08 +0300

    Exercise 1 solution

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from zipfile import ZipFile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -12,8 +14,24 @@ download_uris = [
 
 
 def main():
-    # your code here
-    pass
+    # your code
+    os.makedirs('downloads', exist_ok=True)
+    for url in download_uris:
+
+        file_name = url.split('/')[-1]
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f'Successed to download {url}')
+            with open(f'./downloads/{file_name}', 'wb') as f:
+                f.write(response.content)
+
+            with ZipFile(f'./downloads/{file_name}', 'r') as zobj:
+                zobj.extract(file_name.replace('.zip', '.csv'), './downloads/')
+                zobj.close()
+
+            os.remove(f'./downloads/{file_name}')
+        else:
+            print(f'Failed to download {url}')
 
 
 if __name__ == "__main__":
